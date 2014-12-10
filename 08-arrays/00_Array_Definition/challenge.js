39076035938b724a97659ca550afc78824d7baa1
philipcurra <philipcurra@outlook.com>
2014-12-10 23:08:33 +0000

    ex8

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,7 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(v1, v2, v3, v4) {
+	return([v1,v2,v3,v4]);
+}
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(arr) {
+	return([arr.shift(),arr.pop()]);
+}
