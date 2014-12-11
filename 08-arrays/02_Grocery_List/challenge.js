5842a5476e1e4d67d03aa602b21286e474826c5d
philipcurra <philipcurra@outlook.com>
2014-12-11 16:26:30 +0000

    ex8

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,20 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(obj, arr) {
+	var check = "check";
+	for(i=0; i<arr.length; i++) {
+		if(arr[i]===obj){
+			check = "got it";
+		}
+	}
+	if(check == "got it") {
+		return(arr);
+	}
+	else {
+		arr.push(obj);
+		return(arr);
+	}
+};
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function(arr) {
+	arr.sort();	
+	return(arr.reverse());
+}
