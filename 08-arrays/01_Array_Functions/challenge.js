8d2420d1e8d27ab464e72661a86a4871dd9fad6c
philipcurra <philipcurra@outlook.com>
2014-12-19 22:44:06 +0000

    81

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -3,14 +3,17 @@ module.exports.reversePlusOne = function(arr) {
 	return(arr.reverse());
 }
 
-
 module.exports.plusesEverywhere = function(arr) {
+	return(arr.join([separator='+']));
+}
+
+/*module.exports.plusesEverywhere = function(arr) {
 	temp = arr.shift();
 	for(i=1; i<arr.length+i; i++) {
 		temp = temp + "+" + arr.shift();
 	};
 	return(temp);
-};
+};*/
 
 module.exports.arrayQuantityPlusOne = function(arr) {
 	return(arr.length+1);	
