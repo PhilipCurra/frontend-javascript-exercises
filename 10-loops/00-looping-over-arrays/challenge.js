c62f5b337362081ce0488390610add9b1a6b1454
philipcurra <philipcurra@outlook.com>
2014-12-20 00:19:14 +0000

    10a

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,19 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(arr) {
+	var temp = 0;
+	for(i = 0; i < arr.length; i++) {
+		temp += arr[i];
+	}
+	return temp;
+}
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(inputString) {
+	return(inputString.toLowerCase().split(","));
+}
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(arr) {
+	var retro = [];
+	for(i=0; i<arr.length; i++) {
+		retro[i] = i + " is " + arr[i];
+	}
+	return(retro);
+}
